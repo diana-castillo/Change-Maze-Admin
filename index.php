1ac435c138ea60b3e96f87f02a06369cc32d14ea
Diana <diana.castillo7676@alumnos.udg.mx>
2023-10-01 00:31:19 -0600

    Remover librerias de Phaser

--- index.php
+++ index.php
@@ -13,14 +13,9 @@
 
     <!-- Agregar todos las bibliotecas necesitadas -->
 
-    <!-- game.php -->
      <!-- jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@mikewesthad/dungeon@1.2.2"></script>
-    <script src="https://cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
 
-    <!-- registro.php y encuesta.php -->
     <!-- Bootstrap v5.1.3 CDNs -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
