1c801949f1557ed5952b7ffaae67c4d8cacf9288
Diana <diana.castillo7676@alumnos.udg.mx>
2023-10-29 17:25:25 -0600

    Icono

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChangeMaze Admin</title>
+    <link rel="icon" href="modulos/icono.png" type="image">
 
     <!-- Agregar todos las bibliotecas necesitadas -->
 
